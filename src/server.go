26903841daced46abb03aeba07ed4a98d1a0c08d
eklownr <eklownr@gmail.com>
2026-05-15 21:54:00 +0200

    add mux.Router

--- src/server.go
+++ src/server.go
@@ -3,15 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	route := mux.NewRouter()
+
+    route.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    })
+
+
+	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested the URL path: %s\n", r.URL.Path)
     })
 
 	fs := http.FileServer(http.Dir("static/"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.ListenAndServe(":8888", nil)
+	port := "8888"
+    fmt.Printf("Servern körs på http://localhost:%s\n", port)
+    http.ListenAndServe(":" + port, route)
 }
